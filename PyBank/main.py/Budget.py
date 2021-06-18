bdf1468d4d982b4108d423f4d7901ca1587d9351
Riley Furr <jrfurr7@gmail.com>
2021-06-18 15:43:21 -0400

    Python Project Folders

---
+++ PyBank/main.py/Budget.py
@@ -0,0 +1,43 @@
+import os 
+import csv
+
+csvpath = os.path.join( "Resources", "budget_data.csv")
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
+        #Create lists and print
+dates=[]
+Prof_loss=[]
+for row in csvreader:
+    dates.append(row[0])
+    Prof_loss.append(float(row[1]))
+print(dates)
+print(Prof_loss) 
+
+# #Total Months
+# print(len(dates))
+# Net_Total= sum(Prof_loss)
+# print("$", sum(Prof_loss))
+
+# #Average Change
+# changes=[]
+# for x in range(1,len(dates)):
+#     change = Prof_loss[x]-Prof_loss[x-1]
+#     changes.append(change)
+
+# #Greatest Increase
+# maxchange=max(changes)
+# print(maxchange)
+
+# minchange=min(changes)
+# print(minchange)
+
+
