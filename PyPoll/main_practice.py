a54219da2949a167c2ff7acf396957f26bafac86
Riley Furr <jrfurr7@gmail.com>
2021-06-24 19:43:02 -0400

    Final touches

--- PyPoll/main_practice.py
+++ PyPoll/main_practice.py
@@ -2,6 +2,7 @@ import os
 import csv
 
 path = os.path.join("Resources", "election_data.csv")
+txtpath = os.path.join( "Analysis", "Poll_Analysis.txt")
 
 with open(path) as file:
     csvreader = csv.reader(file)
@@ -13,7 +14,7 @@ with open(path) as file:
 candidates = {}
 #total votes
 total_votes = len(data)
-print(total_votes)
+# print(total_votes)
 
 #list of candidates, unique list using 'set' function
 candidate_votes = []
@@ -21,7 +22,7 @@ for row in data:
     candidate_votes.append(row[2])
 
 unique_candidates = set(candidate_votes)
-print(unique_candidates)
+# print(unique_candidates)
 
 #key in candidates dic
 for candidate in unique_candidates:
@@ -30,13 +31,13 @@ for candidate in unique_candidates:
 #total voters per candidate
 for vote in candidate_votes:
     candidates[vote] += 1
-print(candidates)
+# print(candidates)
 
 #vote percentage for each candidate
-candidates_pct = {}
-for candidate in unique_candidates:
-    vote_count = candidates[candidate]
-    candidates_pct[candidate] = int(round(vote_count/total_votes * 100, 0))
+# candidates_pct = {}
+# for candidate in unique_candidates:
+#     vote_count = candidates[candidate]
+#     candidates_pct[candidate] = int(round(vote_count/total_votes * 100, 0))
 
 
 #winner
@@ -44,11 +45,29 @@ winner_name = ""
 winner_votes = 0
 winner_pct = 0 
 for key, value in candidates.items():
+    print(key, value)
     if value > winner_votes:
         winner_votes = value 
         winner_name = key 
-        winner_pct = candidates_pct[key]
-print(winner_name, winner_votes, winner_pct)
+        # winner_pct = candidates_pct[key]
+# print(winner_name, winner_votes, winner_pct)
+candidates_results = ""
+for candidate in candidates: 
+    # print(candidate)
+    candidates_results = candidates_results + f'{candidate}: {round(candidates[candidate]*100/total_votes, 2)}% ({candidates[candidate]})\n'
+# print(candidates_results)
+
+election_results = f"""Election Results
+------------------------------
+Total Votes: {total_votes}
+------------------------------
+""" + candidates_results + f"""------------------------------
+Election Winner: {winner_name}
+"""
+
+print(election_results)
+with open(txtpath, "w") as txtfile:
+    txtfile.write(election_results)
 
 # #analysis print
 # print("Election Results")
